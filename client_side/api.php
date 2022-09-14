22188eba0924be54b90e7bc4a242eef1bae1a2f7
Luca Somaschini <somas99@gmail.com>
2022-09-14 17:04:57 +0200

    Added comments

--- client_side/api.php
+++ client_side/api.php
@@ -1,7 +1,10 @@
 <?php
+    // recupero dati da database.php;
     include __DIR__ . '/../database.php';
 
+    // specifico che i dati in uscita devono essere letti come json;
     header('Content-Type: application/json');
 
+    // preparo invio dati in formato json;
     echo json_encode($database);
 ?>
