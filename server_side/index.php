70a2f2b6f6b3b3fa5b5b534f8ec57b5ba64540d3
Luca Somaschini <somas99@gmail.com>
2022-09-14 16:57:28 +0200

    Setting up client_side files

--- server_side/index.php
+++ server_side/index.php
@@ -1,6 +1,8 @@
 <?php
+    // richiamo il file php del database
     include __DIR__ . '/../database.php';
 
+    // con un ciclo stampo i dati in pagina
     foreach($database as $album) {
         echo '<ul>';
         echo '<li>' . $album['title'] . ' - ' . $album['author'] . ' - ' . $album['year'] . ' - ' . $album['poster'] . ' - ' . $album['genre'];
