c4ee26abb9e8d8c934727bb483e1964d3adbacd7
Luca Somaschini <somas99@gmail.com>
2022-09-14 18:11:28 +0200

    Added graphics to index.php

--- server_side/index.php
+++ server_side/index.php
@@ -1,11 +1,39 @@
 <?php
-    // richiamo il file php del database
+    // richiamo il file php del database contente gli album che userò nell'html sottostante
     include __DIR__ . '/../database.php';
 
-    // con un ciclo stampo i dati in pagina
-    foreach($database as $album) {
-        echo '<ul>';
-        echo '<li>' . $album['title'] . ' - ' . $album['author'] . ' - ' . $album['year'] . ' - ' . $album['poster'] . ' - ' . $album['genre'];
-        echo '</ul>';
-    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Index.php: php, ajax, dischi</title>
+</head>
+<body>
+
+    <header>
+        <img src="../img/spotify_logo.png" alt="Logo">
+    </header>
+
+    <main>
+        <div class="container">
+            <?php
+                foreach($database as $album) {
+                    echo '<div class="card">';
+                    echo "<img src='" . $album['poster'] . "'" . "alt='" . $album['title'] . "'" . '/>';
+                    echo "<h3>" . $album['title'] . "</h3>";
+                    echo "<h4>" . $album['author'] . "</h4>";
+                    echo "<h5>" . $album['year'] . "</h5>";
+                    echo "<p>" . $album['genre'] . "</p>";
+                    echo '</div>';
+                }
+            ?>
+        </div>
+    </main>
+
+</body>
+</html>
