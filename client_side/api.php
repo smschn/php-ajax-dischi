7c0062f950ad18b893856210704e354846d557b1
Luca Somaschini <somas99@gmail.com>
2022-09-15 16:09:36 +0200

    Bonus: completed

--- client_side/api.php
+++ client_side/api.php
@@ -2,9 +2,23 @@
     // recupero dati da database.php;
     include __DIR__ . '/../database.php';
 
+    // creo array vuoto dove inserire gli album filtrati
+    $albumsFilteredWithGet = [];
+
+    // filtro gli album in base al genere ricevuto come parametro GET
+    if (isset($_GET['genre']) && $_GET['genre'] != '') {
+        foreach($database as $album) {
+            if ($album['genre'] == $_GET['genre']) {
+                $albumsFilteredWithGet[] = $album;
+            }
+        }
+    } else {
+        $albumsFilteredWithGet = $database;
+    }
+
     // specifico che i dati in uscita devono essere letti come json;
     header('Content-Type: application/json');
 
     // preparo invio dati in formato json;
-    echo json_encode($database);
+    echo json_encode($albumsFilteredWithGet);
 ?>
--- client_side/script.js
+++ client_side/script.js
@@ -1,12 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        albums: []
+        albums: [],
+        genres: [],
+        selectedGenre: ''
     },
     mounted() {
-        axios.get('http://localhost/progetti_php/45/php-ajax-dischi/client_side/api.php')
-        .then( response => {
-            this.albums = response.data;
-        });
+        this.getAlbums();
+    },
+    methods: {
+        getAlbums() {
+            axios.get('http://localhost/progetti_php/45/php-ajax-dischi/client_side/api.php' + '?genre=' + this.selectedGenre)
+            .then( response => {
+                this.albums = response.data;
+                if (this.genres.length == 0) {
+                    this.albums.forEach( album => {
+                        if (!this.genres.includes(album.genre)) {
+                            this.genres.push(album.genre);
+                        }
+                    })
+                }
+            })
+        }
     }
 })
